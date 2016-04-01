4bd7de86d7c5eb1e428b1b6143b7ea2f7aaa71df
Nikola Miric <nikolamiric@yahoo.com>
2016-04-01 15:54:49 +0200

    LoginViewController almost completed

--- ToDo/ToDo/LogInViewController.m
+++ ToDo/ToDo/LogInViewController.m
@@ -8,5 +8,45 @@
 
 #import "LogInViewController.h"
 
+@interface LogInViewController ()
+@property (weak, nonatomic) IBOutlet UIImageView *usernameImageView;
+
+@property (weak, nonatomic) IBOutlet UIImageView *passwordImageView;
+
+@property (weak, nonatomic) IBOutlet UITextField *usernameTextField;
+
+@property (weak, nonatomic) IBOutlet UITextField *passwordTextField;
+
+
+@end
+
 @implementation LogInViewController
+#pragma mark - Private API
+- (void)configureTextField:(UITextField*)textField {
+    if (textfield.placeholder.lenght > 0){
+        NSDictionary *atributes = @{
+                                    NSFontAttributeName: [UIFontWithName:@"AvenirNext-Regular" size 14.8],
+                                    NSForcegroundCollorAttributeName: [UIColor whiteCollor]
+                                    };
+        textField.atributedPlacehplder=[[NSAtributedString alloc] initWithString textfield.placeholder atributes:atributes];
+        
+    }
+}
+
+#pragma mark - Actions
+
+- (IBAction)forgotpasswordButtonTapped:(id)sender {
+}
+- (IBAction)SignInButtonTapped:(id)sender {
+}
+
+-(void) viewDidLoad{
+    [super viewDidLoad];
+    [self configureTextField:self.usernameTextField];
+    
+    [self configureTextField:self.passwordTextField];
+    
+}
+
+
 @end
